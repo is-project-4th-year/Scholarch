4a271c093ba39046cba4aae2b37fa1a04ad2e2ab
“Gambii-tech <telvin.muriuki@strathmore.edu>
2025-09-01 13:34:06 +0300

    Issue #3: User Authentication & Auth Screens
    Created login, signup, onboarding, and two-factor authentication screens. Integrated Firebase for user management and authentication. Implemented state management using Zustand to handle user authentication state across the app.

--- Scholarch-app/app/_layout.tsx
+++ Scholarch-app/app/_layout.tsx
@@ -1,5 +1,25 @@
 import { Stack } from "expo-router";
+import { useAuthStore } from "../stores/authStore";
 
 export default function RootLayout() {
-  return <Stack />;
+
+ const { loading, authStatus } = useAuthStore();
+
+    if (loading) {
+        return (
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="index" />
+            </Stack>
+        );
+    }
+
+    
 }
+// Show splash while loading
+//if (loading) return <Index />;
+
+// Then decide: auth screens, onboarding, or app
+//if (authStatus === AuthStatus.AWAITING_EMAIL_VERIFICATION) return <VerifyEmailScreen />;
+//if (authStatus === AuthStatus.ONBOARDING) return <OnboardingLayout />;
+//if (authStatus === AuthStatus.AUTHENTICATED) return <AppLayout />;
+
